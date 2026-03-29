220de2ccfb31b59153132aefbbbd8c854344afcd
Ruslan Kolodich <k0l0dic4rus@gmail.com>
2026-03-29 18:22:39 +0300

    Update posts section header

--- _11ty/collections/index.js
+++ _11ty/collections/index.js
@@ -2,7 +2,7 @@ import { posts, postsByYear } from "./posts.js";
 
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 export function registerCollections(eleventyConfig) {
-	/** Each collection should starts with $ to point that is custom collection */
+	/** Each collection should start with $ to point that is custom collection */
 	eleventyConfig.addCollection("$posts", posts);
 	eleventyConfig.addCollection("$postsByYear", postsByYear);
 }
