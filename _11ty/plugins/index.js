e69238b73bac26ba3d491e30d7a4e2440c2705ba
Ruslan Kolodich <k0l0dic4rus@gmail.com>
2026-04-30 22:09:01 +0300

    Add sitemap and dir output

--- _11ty/plugins/index.js
+++ _11ty/plugins/index.js
@@ -1,5 +1,9 @@
 import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
 import readingtime from "@myxotod/eleventy-plugin-readingtime";
+import sitemap from "@quasibit/eleventy-plugin-sitemap";
+import dirOutput from "@11ty/eleventy-plugin-directory-output";
+
+import { DOMAIN } from "../../_share/const.js";
 
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 export function registerPlugins(eleventyConfig) {
@@ -9,10 +13,26 @@ export function registerPlugins(eleventyConfig) {
 		prefixDisplay: false,
 		verbose: false,
 	});
+
 	eleventyConfig.addPlugin(syntaxHighlight, {
 		preAttributes: {
 			"data-language": ({ language }) => language,
 			tabindex: 0,
+		},
+	});
+
+	eleventyConfig.addPlugin(sitemap, {
+		lastModifiedProperty: "modified",
+		sitemap: {
+			hostname: DOMAIN,
+		},
+	});
+
+	eleventyConfig.setQuietMode(true);
+	eleventyConfig.addPlugin(dirOutput, {
+		columns: {
+			filesize: true,
+			benchmark: true,
 		},
 	});
 }
--- _share/const.js
+++ _share/const.js
@@ -1,3 +1,5 @@
 export const IS_PROD = process.env.NODE_ENV === "prod";
 
 export const ROOT_FOLDER = process.cwd();
+
+export const DOMAIN = IS_PROD ? process.env.DOMAIN : "http://localhost:8800";
